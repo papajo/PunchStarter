d596fc5faa8fcf6e1c1b6b8bff1f6d1522969a9b
papajo <joshipv2@gmail.com>
2016-05-17 12:11:47 -0400

    setup data models, SQLAlchemy

--- punchstarter/__init__.py
+++ punchstarter/__init__.py
@@ -1,8 +1,17 @@
 from flask import Flask, render_template
 from flask.ext.script import Manager
+from flask.ext.sqlalchemy import SQLAlchemy
+from flask.ext.migrate import Migrate, MigrateCommand
 
 app = Flask(__name__)
+app.config.from_object('punchstarter.default_settings')
 manager = Manager(app)
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+manager.add_command('db', MigrateCommand)
+
+from punchstarter.models import *
 
 @app.route("/")
 def hello():
