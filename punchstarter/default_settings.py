f3d656b9219b4613694c1a6f1dcf12c75cbb3b97
papajo <joshipv2@gmail.com>
2016-05-19 10:55:55 -0400

    prep for heroku

--- punchstarter/default_settings.py
+++ punchstarter/default_settings.py
@@ -1,12 +1,16 @@
 import os
 import cloudinary
 
-DEBUG=True
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + BASE_DIR + '/app.db'
+DEBUG = os.environ.get('DEBUG', True)
+SQLALCHEMY_DATABASE_URI=os.environ.get('SQLALCHEMY_DATABASE_URI',"sqlite:///"+ BASE_DIR + "/app.db")
+SQLALCHEMY_TRACK_MODIFICATIONS=os.environ.get('SQLALCHEMY_TRACK_MODIFICATIONS',True)
+CLOUDINARY_CLOUD_NAME=os.environ.get('CLOUDINARY_CLOUD_NAME',"papajo")
+CLOUDINARY_API_KEY=os.environ.get('CLOUDINARY_API_KEY',"222585141824773")
+CLOUDINARY_API_SECRET=os.environ.get('CLOUDINARY_API_SECRET',"Ery2sWQZNsUvo5wqwnESC7yohJY")
 
 cloudinary.config(
-  cloud_name = "papajo",
-  api_key = "222585141824773",
-  api_secret = "Ery2sWQZNsUvo5wqwnESC7yohJY"
+  cloud_name = CLOUDINARY_CLOUD_NAME,
+  api_key = CLOUDINARY_API_KEY,
+  api_secret = CLOUDINARY_API_SECRET
 )
